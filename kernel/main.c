c8d7eaf9c702aa3771f88a82a191121cc2f27a8b
hxh <3148182735@qq.com>
2025-08-10 08:31:06 +0800

    更改main

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,21 @@
-int main(){
+# include "./console/console.h"
 
+
+
+void init();
+
+int main(){
+	init();
+    print_color("Hello %s World!\n %d",COLOR_GREEN , "HXH" , 123);
+	
     while (1){
         
     }
     
     return 0;
+}
+
+
+void init(){
+	init_console(COLOR_WHITE);
 }
